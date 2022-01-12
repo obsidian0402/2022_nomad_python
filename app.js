d1c63ca230183505d05ae3a34f2e1bb08613f27a
sok chan hwi <tjrcksgnl@gmail.com>
2022-01-12 17:11:07 +0900

    오늘꺼

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const a = 10; //변수선언 (constatnt)
+const a = 10; //변수선언 (constatnt : 불변)
 const b = 2;
 const veryLongMyName='hwi'; //대문자 사용하기
 console.log(a+2); 
 console.log(a*2);
 console.log(a/2);
 console.log("hello"+"hwi");
+
+
+let c = 5; //let:변한다, 생성할때만 사용.
+c= 6; //let 으로 
---
+++ function.js
@@ -0,0 +1,13 @@
+function sayHello(name,age){
+    consol.log("hello i'm " + name );
+}
+
+sayHello("hwi", 29);
+
+//계산기 만들기
+function plus(a,b){
+    console.log(a+b);
+}
+
+plus(2,3);
+
